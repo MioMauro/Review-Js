5d1a968a27930b3c56e2b21d8d2addd7f96528a6
Mauro.V <101286831+MioMauro@users.noreply.github.com>
2022-08-31 15:26:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,81 @@
-const reviewContainer = document.getElementById("review-container")
-
 const reviews = [
     {
-        img:"https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
-        alt:"Profile picture of Indyah Almay",
-        text:"I really enjoyed this course! It was fun and interactive as well as educational. I learnt a lot!",
-        name:"Indyah Almay"
+    id:1,
+    name: "Andrea Piacquadio",
+    job: "ux designer", 
+    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg", 
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam nesciunt quas vitae voluptatibus quos a sequi vero molestias perspiciatis possimus!",
+    }, 
+    {
+    id:2, 
+    name:"mentatdgt", 
+    job:"web designer", 
+    img:"https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+    text:"dolor sit amet consectetur, adipisicing elit. Totam nesciunt quas vitae voluptatibus quos a sequi vero molestias perspiciatis possimus!",
     },
     {
-        img:"https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-        alt:"Profile picture of Jonas Kakaroto",
-        text:"A well thought out course with the student in mind. Thanks for creating this for the community",
-        name:"Jonas Kakaroto"
-    },
+    id:3, 
+    name:"Moose Photos", 
+    job:"intern", 
+    img:"https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+    text:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae voluptatibus quos a sequi vero molestias perspiciatis possimus!",
+    }, 
     {
-        img:"https://images.unsplash.com/photo-1548819611-fc0f1732d402?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-        alt:"Franco Figueroa",
-        text:"I managed to do this course in my spare time afte hours and have only positive things to say!",
-        name:"Franco Figueroa"
+    id:4, 
+    name:"Spencer Selover", 
+    job:"teacher", 
+    img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+    text:" Ad nihilum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam nesciunt quas vitae voluptatibus quos a sequi vero molestias perspiciatis possimus!",
+},
+];
+
+// select items
+const img = document.getElementById("person-img")
+const author = document.getElementById("author")
+const job = document.getElementById("job")
+const info = document.getElementById("info")
+
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+const randomBtn = document.querySelector(".random-btn")
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function(){
+    showPerson()
+})
+
+// show person based on item
+function showPerson() {
+    const item = reviews[currentItem]
+    img.src = item.img
+    author.textContent = item.name
+    job.textContent = item.job
+    info.textContent = item.text
+}
+
+// show next person
+nextBtn.addEventListener('click', function(){
+    currentItem++
+    if (currentItem > reviews.length - 1) {
+    currentItem = 0
     }
-]
+    showPerson()
+})
 
-function populateDisplay() {
-    reviews.forEach(review => {
-        const cardElement = document.createElement('div')
-        cardElement.classList.add('card')
+// show previous person
+prevBtn.addEventListener('click', function(){
+    currentItem--
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1    
+    }
+    showPerson()
+})
 
-        cardElement.addEventListener('mouseover', showCard)
-        cardElement.addEventListener('mouseleave', blurCard)
-
-        const imageContainer = document.createElement('div')
-        imageContainer.classList.add('image-container')
-        const imageElement = document.createElement('img')
-        imageElement.setAttribute('src', review.img)
-        imageElement.setAttribute('alt', review.alt)
-        imageContainer.append(imageElement)
-
-        const paragraphElement = document.createElement('p')
-        paragraphElement.textContent = review.text
-        const nameContainer = document.createElement('div')
-        nameContainer.classList.add('name-container')
-        nameContainer.textContent = review.name
-
-        cardElement.append(imageContainer, paragraphElement, nameContainer)
-
-        reviewContainer.append(cardElement)
-    })
-}
-
-populateDisplay()
-
-function showCard () {
-    this.classList.add('active')
-}
-
-function blurCard () {
-    this.classList.remove('active')
-}
+// show random person
+randomBtn.addEventListener('click', function() {
+    currentItem = Math.floor(Math.random() * reviews.length)
+    showPerson()
+})
